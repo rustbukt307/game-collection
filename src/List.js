85d5eee45ebdeabf0895b5377f27b0931362ddfd
Tom Moliterno <rustbukt307@Toms-MacBook-Air.local>
2020-04-29 21:36:27 -0400

    I can't seem to leave this thing alone tonight

--- src/List.js
+++ src/List.js
@@ -9,20 +9,13 @@ class List extends Component {
     const index = parseInt(this.props.match.params.index)
     window.location.replace(this.props.birds[index].homepage)
   }
+
   render() {
 
     const index = (parseInt(this.props.match.params.index))
     return (
       <div className="list">
-        {/* <img src={this.props.birds[index].image}></img>
-        <div className="birdInfo">
-          <h2>{this.props.birds[index].name}</h2>
-          <p>({this.props.birds[index].genus})</p>
-          <p>{this.props.birds[index].conservationStatus}</p>
-          <Link>
-            <button onClick={this.handleClick} type="button"><strong>Read More</strong></button>
-          </Link>
-        </div> */}
+        <h1 style={{textAlign: "center"}}>Coming Soon!</h1>
       </div>
     )
   }
